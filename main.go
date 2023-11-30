150f8b4635a6bba416372927940b44fce9b3026b
黄沛健 <accounts_6374a5d24f6a9c17e2dfccab@mail.teambition.com>
2023-11-30 15:28:36 +0800

    更新 main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
     "strings"
     "sync"
 
-    // Kubernetes 相关的包
+    // Kubernetes 客户端库
     "k8s.io/api/admission/v1beta1"
     v1 "k8s.io/api/core/v1"
     metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -18,20 +18,18 @@ import (
     "k8s.io/client-go/rest"
 )
 
-// PodHostnameTracker 结构体用于追踪和分配 Pod 主机名
+// PodHostnameTracker 是一个结构体，用于跟踪和获取下一个可用的主机名
 type PodHostnameTracker struct {
-    clientset *kubernetes.Clientset // Kubernetes 客户端
-    lock      sync.Mutex            // 保证并发访问的互斥锁
+    clientset *kubernetes.Clientset // Kubernetes 客户端集
+    lock      sync.Mutex            // 用于同步的互斥锁
 }
 
 // NewPodHostnameTracker 创建并返回一个新的 PodHostnameTracker 实例
 func NewPodHostnameTracker() (*PodHostnameTracker, error) {
-    // 获取 Kubernetes 集群配置
     config, err := rest.InClusterConfig()
     if err != nil {
         return nil, err
     }
-    // 创建 Kubernetes 客户端实例
     clientset, err := kubernetes.NewForConfig(config)
     if err != nil {
         return nil, err
@@ -39,12 +37,12 @@ func NewPodHostnameTracker() (*PodHostnameTracker, error) {
     return &PodHostnameTracker{clientset: clientset}, nil
 }
 
-// GetNextHostname 根据当前在特定命名空间和部署下的 Pod，生成下一个可用的主机名
+// GetNextHostname 为指定的命名空间和部署名获取下一个可用的主机名
 func (tracker *PodHostnameTracker) GetNextHostname(namespace, deploymentName string) (string, error) {
-    tracker.lock.Lock() // 加锁以保证线程安全
-    defer tracker.lock.Unlock() // 函数结束时解锁
+    tracker.lock.Lock()
+    defer tracker.lock.Unlock()
 
-    // 获取指定命名空间和标签的所有 Pod
+    // 获取指定部署的所有 Pods
     pods, err := tracker.clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{
         LabelSelector: "app=" + deploymentName,
     })
@@ -52,27 +50,39 @@ func (tracker *PodHostnameTracker) GetNextHostname(namespace, deploymentName str
         return "", err
     }
 
-    // 分析现有 Pod 的主机名，记录已经使用的编号
     usedNumbers := make(map[int]bool)
     for _, pod := range pods.Items {
-        if parts := strings.Split(pod.Name, "-"); len(parts) > 1 {
-            if num, err := strconv.Atoi(parts[len(parts)-1]); err == nil {
+        hostname := pod.Spec.Hostname
+    
+        // 如果 hostname 设置了，提取并标记使用过的编号
+        if hostname != "" {
+            trimmedHostname := strings.TrimPrefix(hostname, deploymentName+"-")
+            num, err := strconv.Atoi(trimmedHostname)
+            if err == nil {
                 usedNumbers[num] = true
             }
         }
     }
-
-    // 找到未使用的最小编号
-    for i := 1; i <= 50; i++ {
+    
+    // 检查编号 1 是否已被使用
+    if !usedNumbers[1] {
+        // 如果编号 1 没有被使用，直接返回它
+        return fmt.Sprintf("%s-%d", deploymentName, 1), nil
+    }
+    
+    // 如果编号 1 已被使用，查找下一个可用编号
+    for i := 2; i <= 50; i++ {
         if !usedNumbers[i] {
             return fmt.Sprintf("%s-%d", deploymentName, i), nil
         }
     }
-
+    
+    // 如果找不到可用编号，返回错误
     return "", fmt.Errorf("no available hostname number found")
+    
 }
 
-// handleMutate 处理 Kubernetes 准入控制器的 Webhook 请求
+// handleMutate 处理变更请求，为 Pod 设置唯一的主机名
 func handleMutate(tracker *PodHostnameTracker, w http.ResponseWriter, r *http.Request) {
     var review v1beta1.AdmissionReview
     if err := json.NewDecoder(r.Body).Decode(&review); err != nil {
@@ -80,16 +90,14 @@ func handleMutate(tracker *PodHostnameTracker, w http.ResponseWriter, r *http.Re
         return
     }
 
-    // 解析请求中的 Pod 对象
     var pod v1.Pod
     if err := json.Unmarshal(review.Request.Object.Raw, &pod); err != nil {
         http.Error(w, fmt.Sprintf("unmarshal error: %v", err), http.StatusBadRequest)
         return
     }
 
-    // 获取 Pod 的部署名称
+    // 获取部署名，并生成新的主机名
     deploymentName := pod.GetLabels()["app"]
-    // 生成新的主机名
     hostname, err := tracker.GetNextHostname(pod.Namespace, deploymentName)
     if err != nil {
         http.Error(w, fmt.Sprintf("error getting next hostname: %v", err), http.StatusInternalServerError)
@@ -110,7 +118,7 @@ func handleMutate(tracker *PodHostnameTracker, w http.ResponseWriter, r *http.Re
         return
     }
 
-    // 构造准入控制响应
+    // 设置返回的 AdmissionReview 响应
     review.Response = &v1beta1.AdmissionResponse{
         UID:       review.Request.UID,
         Allowed:   true,
@@ -127,21 +135,19 @@ func handleMutate(tracker *PodHostnameTracker, w http.ResponseWriter, r *http.Re
     }
 }
 
-// 主函数
+// main 函数设置 HTTP 服务器和处理路由
 func main() {
-    // 初始化 PodHostnameTracker
     tracker, err := NewPodHostnameTracker()
     if err != nil {
         fmt.Printf("Failed to initialize hostname tracker: %v\n", err)
         return
     }
 
-    // 设置 HTTP 路由和处理函数
+    // 设置 /mutate 路径的处理函数
     http.HandleFunc("/mutate", func(w http.ResponseWriter, r *http.Request) {
         handleMutate(tracker, w, r)
     })
     fmt.Println("Starting webhook server...")
-
     // 启动 HTTPS 服务器
     if err := http.ListenAndServeTLS(":8443", "/app/tls.crt", "/app/tls.key", nil); err != nil {
         fmt.Printf("Failed to start server: %v\n", err)
